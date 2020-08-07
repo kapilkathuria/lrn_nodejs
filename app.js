d854caaee8215884192164a23ef2b09847bf968b
Kapil Kathuria <kapil.kathuria@gmail.com>
2020-08-07 06:27:02 +0530

    sectio3 - routingn in progress

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // 
 
 const http = require('http');
+const fs = require('fs');
 
 // Function can be called from createServer in either of following ways
 // Method 1 - create separate function
@@ -12,11 +13,28 @@ const http = require('http');
 // http.createServer(function(req, res){
 // Method 3 - call as follows - all means samething
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    method = req.method;
+    if (url === '/'){
+        res.write('<html')
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><h1>URL is root</h1></body>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html')
+        return res.end()
+    }
+    if (url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt','dummy text');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Header','text/html');
     res.write('<html')
     res.write('<head><title>This is title</title></head>')
-    res.write('<body><h1>This is body</h1></body>')
+    res.write('<body><h1>URL is not root / </h1></body>')
     res.write('</html')
     res.end()
 });
@@ -27,3 +45,5 @@ server.listen(3000);
 // createServer.listen is event listner which keeps on listening event
 // this is event driven approach
 // nnodejs uses event driven apppraoch for everything
+
+// Routing
