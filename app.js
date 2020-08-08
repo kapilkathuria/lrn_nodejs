43336fefceeb5464408cf85bbbf1d85d839c3989
kapil <kapil.kathuria@gmail.com>
2020-08-08 08:51:46 +0530

    different routes creaed from app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require('http');
 
 // express related packages
 const express = require ('express');
+const bodyParser = require('body-parser');
 
 // other third party packges
 
@@ -11,22 +12,29 @@ const express = require ('express');
 // crate app
 const app = express();
 
-// use method allows us add middleware. app.use code block is middleware
-// this wil be called for all incoming requests
-// next: is anothe rfunction which will be tel lwhich next middleware need to be executed
-//  unless next is called code remains in current code block
-app.use((req, res, next) => {
-    console.log ('in middleware');
-    next();  //allows next middleware to be called otherwise code execution remain sin current middleware
-})
+// ths is for parsing - sent, form ect.
+// differnt parser will be used for parsing files  json etc.
+app.use(express.urlencoded({ extended: false }));
 
+// Routing - app.use('/route) - is usesd for routing
 // res.send closes the middleware part and response is sent out 
 //  are res.send as wll code won't move to next code block.
-app.use((req, res, next) => {
-    console.log ('in another middleware');
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+// app.get is same as app.use but will  work only when get request is fired
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    // you will undefined for above log unless 'body-parser' pkg is used
+    // rediects to /
+    res.redirect('/')
+});
+
+// rember '/' matches all i.e. it also matches /add-product
+//  but since respnse is sent from above, code never reches below code block
+app.use('/', (req, res, next) => {
     res.send('<h1>hello from express</h1>');
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
--- section5.js
+++ section5.js
@@ -20,6 +20,24 @@
 // i.e. multiple code blocks can run together
 //  see app.js
 
+// 3. working wiht requests and responses
+// see following in app.js
+// body-parser
+// console.log(req.body);
+
+// 4. routing
+// 
+
+
+// use method allows us add middleware. app.use code block is middleware
+// this wil be called for all incoming requests
+// next: is anothe rfunction which will be tel lwhich next middleware need to be executed
+//  unless next is called code remains in current code block
+app.use((req, res, next) => {
+    console.log ('in middleware');
+    next();  //allows next middleware to be called otherwise code execution remain sin current middleware
+})
+
 
 // Files changes
 // routes.js has been renamed as routes_section4.js, app.js as app_section.js
