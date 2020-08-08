9be5561b85d8f492ead4f30425857f9f43358bb0
kapil <kapil.kathuria@gmail.com>
2020-08-08 09:04:03 +0530

    Added Express Router and code moved to different files

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require ('express');
 const bodyParser = require('body-parser');
 
 // other third party packges
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 // my internal packages
 
@@ -16,25 +18,7 @@ const app = express();
 // differnt parser will be used for parsing files  json etc.
 app.use(express.urlencoded({ extended: false }));
 
-// Routing - app.use('/route) - is usesd for routing
-// res.send closes the middleware part and response is sent out 
-//  are res.send as wll code won't move to next code block.
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-// app.get is same as app.use but will  work only when get request is fired
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    // you will undefined for above log unless 'body-parser' pkg is used
-    // rediects to /
-    res.redirect('/')
-});
-
-// rember '/' matches all i.e. it also matches /add-product
-//  but since respnse is sent from above, code never reches below code block
-app.use('/', (req, res, next) => {
-    res.send('<h1>hello from express</h1>');
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
