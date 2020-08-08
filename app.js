19a5020b728be43309a7ddc1de8a61e661837e1f
kapil <kapil.kathuria@gmail.com>
2020-08-08 09:15:39 +0530

    Added filtering wit express

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ const app = express();
 // differnt parser will be used for parsing files  json etc.
 app.use(express.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
