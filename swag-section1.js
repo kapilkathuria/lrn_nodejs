c0959d69dc95d6e3692c5b1a0b7cd6fbaefb1edb
kapil <kapil.kathuria@gmail.com>
2020-08-08 18:01:32 +0530

    won't be contiuing swagger course here

---
+++ swag-section1.js
@@ -0,0 +1,10 @@
+// https://www.udemy.com/course/nodejs-api-development-with-swagger/learn/lecture/18187422#overview
+
+// Covers
+// using open api
+// use oas 
+// combine oas, swagger and nodejs
+// swagger architecture
+// create rest api with swagger
+// deploy and mange api with security
+
